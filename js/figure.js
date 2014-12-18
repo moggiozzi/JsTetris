8b17a4680f65a17d7daeac8d0b0009a305a93a6e
moggiozzi <moggiozzi@gmail.com>
2014-12-18 10:01:45 +0300

    Код разделен по разным файлам

---
+++ js/figure.js
@@ -0,0 +1,71 @@
+﻿var FIGURE_CODE = { CUBE: 0, LINE: 1, VISOR1: 2, VISOR2: 3, STAIR1: 4, STAIR2: 5, LEDGE: 6 };
+var MAX_FIGURE_CODE = 7
+function Figure(figureCode, colorCode) {
+    this.code = figureCode;
+    this.colorCode = colorCode;
+    // координаты появления
+    this.j = Math.floor((M - 3) / 2);
+    this.i = 0; // todo -2
+    var c = colorCode;
+    switch (figureCode) {
+        case FIGURE_CODE.LINE:
+            this.data = [
+                [0, c, 0],
+                [0, c, 0],
+                [0, c, 0],
+            ];
+            break;
+        case FIGURE_CODE.VISOR1:
+            this.data = [
+                [0, c, c],
+                [0, c, 0],
+                [0, c, 0],
+            ];
+            break;
+        case FIGURE_CODE.VISOR2:
+            this.data = [
+                [c, c, 0],
+                [0, c, 0],
+                [0, c, 0],
+            ];
+            break;
+        case FIGURE_CODE.STAIR1:
+            this.data = [
+                [0, 0, c],
+                [0, c, c],
+                [0, c, 0],
+            ];
+            break;
+        case FIGURE_CODE.STAIR2:
+            this.data = [
+                [c, 0, 0],
+                [c, c, 0],
+                [0, c, 0],
+            ];
+            break;
+        case FIGURE_CODE.LEDGE:
+            this.data = [
+                [0, c, 0],
+                [0, c, c],
+                [0, c, 0],
+            ];
+            break;
+            //case FIGURE_CODE.CUBE:
+        default:
+            this.data = [
+                [0, 0, 0],
+                [0, c, c],
+                [0, c, c],
+            ];
+            break;
+    }
+}
+function copyFigure(figureDst, figureSrc) {
+    figureDst.code = figureSrc.code;
+    figureDst.j = figureSrc.j;
+    figureDst.i = figureSrc.i;
+    var i, j;
+    for (i = 0; i < 3; ++i)
+        for (j = 0; j < 3; ++j)
+            figureDst.data[i][j] = figureSrc.data[i][j];
+}
---
+++ js/sound.js
@@ -0,0 +1,47 @@
+﻿function initAudio() {
+    if (getBrowserName() == "safari") // for Lg WebOS
+    {
+        isWebKit = true;
+        window.context;
+        window.bufferLoader;
+        window.AudioContext = window.AudioContext || window.webkitAudioContext;
+        context = new AudioContext();
+        bufferLoader = new BufferLoader(
+            context,
+            [
+                './sounds/move.wav',
+                './sounds/drop.wav',
+                './sounds/clear.wav',
+            ],
+            function () { }
+        );
+        bufferLoader.load();
+    }
+}
+var isWebKit = false;
+function playSound(id) {
+    if (isWebKit) {
+        playSoundForWebKit(id);
+        return;
+    }
+    var element = document.getElementById(id);
+    if (sounds[id] == undefined) {
+        sounds[id] = new Audio("./sounds/" + id + ".wav");
+        sounds[id].load();
+    }
+    if (sounds[id].played.length == 0 || sounds[id].ended)
+        sounds[id].play();
+}
+
+function playSoundForWebKit(id) {
+    var source = context.createBufferSource(); // может не стоит каждый раз вызывать?
+    var idx = 0;
+    switch (id) { // fixme сделать список звуков
+        case 'move': idx = 0; break;
+        case 'drop': idx = 1; break;
+        case 'clear': idx = 2; break;
+    }
+    source.buffer = window.bufferLoader.bufferList[idx];
+    source.connect(context.destination);
+    source.start(0);
+}
